6e49c605c2ee5e1e109fcb2dad6da69091ef390b
Jonathan Forney <jonathan.forney@gmail.com>
2019-09-06 21:39:21 -0700

    added thing back in

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
-import Auth0Provider from './Auth0Provider';
+import Auth0Provider from './Auth0Propvider';
 
 render(
   <Auth0Provider
